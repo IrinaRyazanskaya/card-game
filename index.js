e5f5166c4a16e3a8c79d23320d50fe93d08799ac
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2025-10-19 05:19:51 +0500

    Уборка в репозитории (#1)
    * Удалила настройки Vercel
    
    * Настроила инструменты разработки
    
    * Отформатировала код с помощью Prettier
    
    * Исправила ошибки найденные линтером
    
    * Написала README
    
    * Исправила заголовок

--- index.js
+++ index.js
@@ -1,93 +1,93 @@
-const menuElement = document.getElementById('menu');
-const gameElement = document.getElementById('game');
-const gameCardsElement = document.getElementById('game-cards');
-const startButtonElement = document.getElementById('start-button');
+const menuElement = document.getElementById("menu");
+const gameElement = document.getElementById("game");
+const gameCardsElement = document.getElementById("game-cards");
+const startButtonElement = document.getElementById("start-button");
 let isCardOpen = false;
 
 function createCard(isBuggy) {
-    const cardElement = document.createElement('div');
-    const backCardElement = document.createElement('div');
-    const frontCardElement = document.createElement('div');
-    cardElement.classList.add('game__card');
-    backCardElement.classList.add('game__card-back');
-    frontCardElement.classList.add('game__card-front');
+  const cardElement = document.createElement("div");
+  const backCardElement = document.createElement("div");
+  const frontCardElement = document.createElement("div");
+  cardElement.classList.add("game__card");
+  backCardElement.classList.add("game__card-back");
+  frontCardElement.classList.add("game__card-front");
 
-    if (isBuggy) {
-        frontCardElement.classList.add('game__card-front_buggy');
-    }
+  if (isBuggy) {
+    frontCardElement.classList.add("game__card-front_buggy");
+  }
 
-    cardElement.appendChild(frontCardElement);
-    cardElement.appendChild(backCardElement);
+  cardElement.appendChild(frontCardElement);
+  cardElement.appendChild(backCardElement);
 
-    return cardElement;
+  return cardElement;
 }
 
 function createCards(count) {
-    const fragment = document.createDocumentFragment();
-    const buggyCard = getBuggyCardNumber(count);
+  const fragment = document.createDocumentFragment();
+  const buggyCard = getBuggyCardNumber(count);
 
-    for (let i = 0; i < count; i++) {
-        const cardElement = createCard(i === buggyCard);
+  for (let i = 0; i < count; i++) {
+    const cardElement = createCard(i === buggyCard);
 
-        cardElement.addEventListener('click', () => {
-            cardElement.classList.add('rotate');
-            if (isCardOpen) {
-                finishGame();
-            } else {
-                isCardOpen = true;
-            }
-        });
+    cardElement.addEventListener("click", () => {
+      cardElement.classList.add("rotate");
+      if (isCardOpen) {
+        finishGame();
+      } else {
+        isCardOpen = true;
+      }
+    });
 
-        fragment.appendChild(cardElement);
-    }
+    fragment.appendChild(cardElement);
+  }
 
-    return fragment;
+  return fragment;
 }
 
 function getBuggyCardNumber(count) {
-    return Math.floor(Math.random() * Math.floor(count));
+  return Math.floor(Math.random() * Math.floor(count));
 }
 
 function renderEasyLevel() {
-    gameCardsElement.classList.add('game__cards_thin');
-    gameCardsElement.appendChild(createCards(3));
+  gameCardsElement.classList.add("game__cards_thin");
+  gameCardsElement.appendChild(createCards(3));
 }
 
 function renderMediumLevel() {
-    gameCardsElement.classList.add('game__cards_thin');
-    gameCardsElement.appendChild(createCards(6));
+  gameCardsElement.classList.add("game__cards_thin");
+  gameCardsElement.appendChild(createCards(6));
 }
 
 function renderHardLevel() {
-    gameCardsElement.appendChild(createCards(10));
+  gameCardsElement.appendChild(createCards(10));
 }
 
 function startGame() {
-    const levelRadioGroup = document.querySelector('.menu__item[name="level"]:checked');
-    const level = levelRadioGroup.value;
+  const levelRadioGroup = document.querySelector('.menu__item[name="level"]:checked');
+  const level = levelRadioGroup.value;
 
-    menuElement.classList.add('hidden');
-    gameElement.classList.remove('hidden');
+  menuElement.classList.add("hidden");
+  gameElement.classList.remove("hidden");
 
-    switch (level) {
-        case 'easy': 
-            renderEasyLevel();
-            break;
-        case 'medium':
-            renderMediumLevel();
-            break;
-        case 'hard':
-            renderHardLevel();
-            break;
-    }
+  switch (level) {
+    case "easy":
+      renderEasyLevel();
+      break;
+    case "medium":
+      renderMediumLevel();
+      break;
+    case "hard":
+      renderHardLevel();
+      break;
+  }
 }
 
-startButtonElement.addEventListener('click', startGame);
+startButtonElement.addEventListener("click", startGame);
 
 function finishGame() {
-    gameElement.classList.add('hidden');
-    menuElement.classList.remove('hidden');
-    gameCardsElement.classList.remove('game__cards_thin');
-    gameCardsElement.innerHTML = '';
-    isCardOpen = false;
+  gameElement.classList.add("hidden");
+  menuElement.classList.remove("hidden");
+  gameCardsElement.classList.remove("game__cards_thin");
+  gameCardsElement.innerHTML = "";
+  isCardOpen = false;
 }
